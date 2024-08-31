a60c2e4aa63796f7abbc88cb94a9b8cd699fc170
永吉勇斗 <y.t.n1110.77@gmail.com>
2024-08-31 22:45:12 +0900

    prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+	plugins: ['prettier-plugin-tailwindcss'],
+	useTabs: true,
+	singleQuote: true,
+	printWidth: 90,
+	tabWidth: 4,
+	jsxSingleQuote: true,
+	bracketSpacing: true,
+	htmlWhitespaceSensitivity: 'ignore',
+	singleAttributePerLine: true,
+};
