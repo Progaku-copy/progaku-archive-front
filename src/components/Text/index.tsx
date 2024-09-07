804414f67ed532e4856090cda5480f00de6c46f9
永吉勇斗 <y.t.n1110.77@gmail.com>
2024-09-07 11:31:51 +0900

    Textコンポーネント実装

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,9 +1,18 @@
+const FONT_SIZES = {
+	l: 'text-xl',
+	m: 'text-base',
+	s: 'text-xs',
+} as const;
+
 type Props = {
 	children: string;
+	fontSize?: keyof typeof FONT_SIZES;
 };
 
-const Text = ({ children }: Props) => {
-	return <div className='aspect-square bg-sky-500'>{children}</div>;
+const Text = ({ children, fontSize = 'm' }: Props) => {
+	return (
+		<div className={`font-noto ${FONT_SIZES[fontSize]} text-black`}>{children}</div>
+	);
 };
 
 export default Text;
