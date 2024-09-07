84948f881b119671df7e464f413ca8c0ac638fd9
永吉勇斗 <y.t.n1110.77@gmail.com>
2024-09-07 22:05:12 +0900

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const { iconsPlugin, getIconCollections } = require("@egoist/tailwindcss-icons")
+
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}', './stories/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
 			colors: {
-        'light-gray': '#B3B3B3',
-      },
+				'light-gray': '#B3B3B3',
+			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		iconsPlugin({
+			collections: getIconCollections(['mdi']),
+		}),
+	],
 };
