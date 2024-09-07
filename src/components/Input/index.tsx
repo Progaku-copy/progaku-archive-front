2dce1ca20783995a12b7bd31974cb69cc9b513b2
永吉勇斗 <y.t.n1110.77@gmail.com>
2024-09-07 11:32:23 +0900

    Input調整

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,26 +1,27 @@
 import { useState } from 'react';
+import Text from '../Text';
 
 type Props = {
-  placeholder: string;
-  label: string;
+	placeholder: string;
+	label: string;
 };
 
 const Input = ({ placeholder, label }: Props) => {
-  const [value, setValue] = useState('');
+	const [value, setValue] = useState('');
 
-  return (
-    <div>
-      <label className='pl-2'>{label}</label>
-      <div>
-        <input
-          className='h-11 border-2 border-light-gray rounded-lg pl-2 w-full'
-          placeholder={placeholder}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        ></input>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<Text>{label}</Text>
+			<div>
+				<input
+					className='h-11 w-full rounded-lg border-2 border-light-gray px-2'
+					placeholder={placeholder}
+					value={value}
+					onChange={(e) => setValue(e.target.value)}
+				></input>
+			</div>
+		</div>
+	);
 };
 
 export default Input;
