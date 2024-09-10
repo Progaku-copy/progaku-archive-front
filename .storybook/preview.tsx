21e02b66fdbbe0b718edefd961954d4e3e2daf4c
永吉勇斗 <y.t.n1110.77@gmail.com>
2024-09-10 23:29:09 +0900

    storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,6 +2,29 @@ import type { Preview } from '@storybook/react';
 import React from 'react';
 import '../src/app/globals.css';
 import { Noto_Sans_JP } from 'next/font/google';
+import { action } from '@storybook/addon-actions';
+import { fn } from '@storybook/test';
+
+const defaultNavigationContext = {
+	push: fn((...args: any[]) => {
+		action('nextNavigation.push')(...args);
+	}),
+	replace: fn((...args: any[]) => {
+		action('nextNavigation.replace')(...args);
+	}),
+	forward: fn((...args: any[]) => {
+		action('nextNavigation.forward')(...args);
+	}),
+	back: fn((...args: any[]) => {
+		action('nextNavigation.back')(...args);
+	}),
+	prefetch: fn((...args: any[]) => {
+		action('nextNavigation.prefetch')(...args);
+	}),
+	refresh: fn(() => {
+		action('nextNavigation.refresh')();
+	}),
+};
 
 const noto = Noto_Sans_JP({
 	display: 'swap',
@@ -11,6 +34,10 @@ const noto = Noto_Sans_JP({
 
 const preview: Preview = {
 	parameters: {
+		nextjs: {
+			appDirectory: true,
+			navigation: defaultNavigationContext,
+		},
 		controls: {
 			matchers: {
 				color: /(background|color)$/i,
