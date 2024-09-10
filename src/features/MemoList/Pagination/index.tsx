c6df5c3bfe53751cfe9883941b40bb9b88eb2f26
永吉勇斗 <y.t.n1110.77@gmail.com>
2024-09-10 23:36:01 +0900

    Paginationコンポーネント

--- src/features/MemoList/Pagination/index.tsx
+++ src/features/MemoList/Pagination/index.tsx
@@ -1,37 +1,140 @@
+'use client';
+
 import Text from '@/components/Text';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
 
 type Props = {
-	children: string;
+	totalPageNumber: number;
 };
 
-const Pagination = ({ children }: Props) => {
+const Pagination = ({ totalPageNumber }: Props) => {
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const pageNumber = searchParams.get('page') || '1';
+
+	const currentPage = Number(pageNumber);
+
+	const createQueryString = useCallback(
+		(name: string, value: string) => {
+			const params = new URLSearchParams(searchParams.toString());
+			params.set(name, value);
+
+			return params.toString();
+		},
+		[searchParams],
+	);
+
+	const handleClickFirstPageButton = () => {
+		if (pageNumber === '1') return;
+		router.push(pathname + '?' + createQueryString('page', '1'));
+	};
+
+	const handleClickPreviousPageButton = () => {
+		if (pageNumber === '1') return;
+		router.push(
+			pathname +
+				'?' +
+				createQueryString('page', (Number(pageNumber) - 1).toString()),
+		);
+	};
+
+	const handleClickNextPageButton = () => {
+		if (pageNumber === totalPageNumber.toString()) return;
+		router.push(
+			pathname +
+				'?' +
+				createQueryString('page', (Number(pageNumber) + 1).toString()),
+		);
+	};
+
+	const handleClickLastPageButton = () => {
+		if (pageNumber === totalPageNumber.toString()) return;
+		router.push(
+			pathname + '?' + createQueryString('page', totalPageNumber.toString()),
+		);
+	};
+
+	const handleClickPageNumber = (page: number) => {
+		router.push(pathname + '?' + createQueryString('page', page.toString()));
+	};
+
+	const generatePagination = () => {
+		if (totalPageNumber <= 10) {
+			return Array.from({ length: totalPageNumber }, (_, i) => i + 1);
+		}
+		if (currentPage <= 4) {
+			return [1, 2, 3, 4, 5, 6, '...', totalPageNumber];
+		}
+		if (currentPage >= totalPageNumber - 4) {
+			return [
+				1,
+				'...',
+				totalPageNumber - 5,
+				totalPageNumber - 4,
+				totalPageNumber - 3,
+				totalPageNumber - 2,
+				totalPageNumber - 1,
+				totalPageNumber,
+			];
+		}
+		return [
+			1,
+			'...',
+			currentPage - 2,
+			currentPage - 1,
+			currentPage,
+			currentPage + 1,
+			currentPage + 2,
+			'...',
+			totalPageNumber,
+		];
+	};
+
 	return (
-		<div>
-			<button className='transitions hover:opacity-75'>
+		<div className='flex items-center'>
+			<button
+				onClick={handleClickFirstPageButton}
+				className='transitions flex h-10 w-10 items-center justify-center hover:opacity-75'
+			>
 				<span className='i-ic-sharp-skip-previous'></span>
 			</button>
-			<button className='transitions hover:opacity-75'>
+			<button
+				onClick={handleClickPreviousPageButton}
+				className='transitions flex h-10 w-10 items-center justify-center hover:opacity-75'
+			>
 				<span className='i-ic-baseline-chevron-left'></span>
 			</button>
-			<button>
-				<Text>1</Text>
-			</button>
-			<button>
-				<Text>2</Text>
-			</button>
-			<button>
-				<Text>3</Text>
-			</button>
-			<button>
-				<Text>4</Text>
-			</button>
-			<button>
-				<Text>5</Text>
-			</button>
-			<button className='transitions hover:opacity-75'>
+			{generatePagination().map((page, index) =>
+				typeof page === 'string' ? (
+					<span key={index}>{page}</span>
+				) : (
+					<button
+						key={index}
+						className={
+							currentPage === page
+								? 'h-10 w-10 rounded-full bg-[#2196F3]'
+								: 'h-10 w-10 rounded-full transition hover:opacity-75'
+						}
+						onClick={() => handleClickPageNumber(page)}
+					>
+						<Text color={currentPage === page ? 'white' : 'black'}>
+							{page}
+						</Text>
+					</button>
+				),
+			)}
+			<button
+				onClick={handleClickNextPageButton}
+				className='transitions flex h-10 w-10 items-center justify-center hover:opacity-75'
+			>
 				<span className='i-ic-baseline-chevron-right'></span>
 			</button>
-			<button className='transitions hover:opacity-75'>
+			<button
+				onClick={handleClickLastPageButton}
+				className='transitions flex h-10 w-10 items-center justify-center hover:opacity-75'
+			>
 				<span className='i-ic-baseline-skip-next'></span>
 			</button>
 		</div>
