2e784cb6d3d7155dc01a808b510bce8133dc745c
永吉勇斗 <y.t.n1110.77@gmail.com>
2024-09-10 23:30:50 +0900

    storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,7 +8,7 @@ const config: StorybookConfig = {
 		'@storybook/addon-essentials',
 		'@chromatic-com/storybook',
 		'@storybook/addon-interactions',
-		'@storybook/addon-queryparams'
+		'@storybook/addon-queryparams',
 	],
 	framework: {
 		name: '@storybook/nextjs',
