362a74c5bce741f1ba3877ddc7dffef5be52f054
rikuriku1234 <riku-aoyama@fundasta.co.jp>
2024-09-16 20:29:22 +0900

    TextAreaコンポーネント作成

---
+++ src/components/TextArea/TextArea.stories.tsx
@@ -0,0 +1,16 @@
+import TextArea from '.';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+    title: 'components/TextArea',
+    component: TextArea,
+} satisfies Meta<typeof TextArea>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        placeholder: '本文',
+    },
+};
---
+++ src/components/TextArea/index.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+type Props = {
+    placeholder?: string;
+};
+
+const TextArea: React.FC<Props> = ({ placeholder }) => {
+    const [value, setValue] = useState('');
+
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setValue(e.target.value);
+    };
+
+    return (
+        <div>
+            <textarea
+                className="w-full h-[382px] border-2 border-light-gray rounded-lg p-2 resize-none"
+                placeholder={placeholder}
+                value={value}
+                onChange={handleChange}
+            />
+        </div>
+    );
+};
+
+export default TextArea;
