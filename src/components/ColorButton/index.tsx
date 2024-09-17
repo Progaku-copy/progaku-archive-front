5e2f444786eaf03854cd9c0d2264df17a01d1f2b
永吉勇斗 <y.t.n1110.77@gmail.com>
2024-09-17 20:55:07 +0900

    ColorButtonコンポーネント

--- src/components/ColorButton/index.tsx
+++ src/components/ColorButton/index.tsx
@@ -1,22 +1,20 @@
+'use client';
 import Text from '../Text';
 
-const BACKGROUD_COLORS = {
+const BACKGROUND_COLORS = {
 	green: 'bg-green',
 	blue: 'bg-blue',
 };
 
 type Props = {
-	color: keyof typeof BACKGROUD_COLORS;
+	color: keyof typeof BACKGROUND_COLORS;
 	label: string;
 };
 
 const ColorButton = ({ color, label }: Props) => {
-	// NOTE: こうするぐらいなら別コンポーネントに分けたほうが良さそうだが、ボタンの数が少ないので一旦これで対応
-	const paddingInline = label === '送信' ? 'px-9' : 'px-3.5';
-
 	return (
 		<button
-			className={`${BACKGROUD_COLORS[color]} rounded ${paddingInline} py-3 text-white transition hover:opacity-75`}
+			className={`${BACKGROUND_COLORS[color]} w-28 rounded py-3 transition hover:opacity-75`}
 			onClick={() => console.log('click button')}
 		>
 			<Text color='white'>{label}</Text>
