e62414f37152ccbad95241c250eea514ad952a45
Conte0745 <r92720473@gmail.com>
2024-09-21 15:50:45 +0900

    タイポ修正

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -5,13 +5,13 @@ import Text from '../Text';
 type Props = {
 	label?: string;
 	iconType: 'edit' | 'delete';
-	onclickHandler: (...args: unknown[]) => unknown;
+	onClickHandler: (...args: unknown[]) => unknown;
 };
 
-const IconButton = ({ label, iconType, onclickHandler }: Props) => {
+const IconButton = ({ label, iconType, onClickHandler }: Props) => {
 	return (
 		<button
-			onClick={onclickHandler}
+			onClick={onClickHandler}
 			className='flex h-full items-center'
 		>
 			<span className={`i-ic-${iconType} m-0.5 h-8 w-8 p-2`}></span>
