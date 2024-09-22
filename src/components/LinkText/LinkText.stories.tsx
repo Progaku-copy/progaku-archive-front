577e54acf6ccca90ce5002d4f990613bfb900da0
永吉勇斗 <y.t.n1110.77@gmail.com>
2024-09-22 22:45:38 +0900

    自動補完

--- src/components/LinkText/LinkText.stories.tsx
+++ src/components/LinkText/LinkText.stories.tsx
@@ -2,34 +2,32 @@ import LinkText from '.';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
-    title: 'components/LinkText',
-    component: LinkText,
-    argTypes: {
-        children: { control: 'text' },
-        href: { control: 'text' },
-    },
-    parameters: {
-        backgrounds: {
-            default: 'lightBlue',
-            values: [
-                { name: 'lightBlue', value: '#1E90FF' },
-            ],
-        },
-    },
+	title: 'components/LinkText',
+	component: LinkText,
+	argTypes: {
+		children: { control: 'text' },
+		href: { control: 'text' },
+	},
+	parameters: {
+		backgrounds: {
+			default: 'lightBlue',
+			values: [{ name: 'lightBlue', value: '#1E90FF' }],
+		},
+	},
 } satisfies Meta<typeof LinkText>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
-        children: 'メモ一覧',
-        href: '/',
-    },
-    render: (args) => (
-        <div className="flex space-x-10">
-            <LinkText {...args} />
-            <LinkText href="/">メモ作成</LinkText>
-        </div>
-    ),
+	args: {
+		children: 'メモ一覧',
+		href: '/',
+	},
+	render: (args) => (
+		<div className='flex space-x-10'>
+			<LinkText {...args} />
+			<LinkText href='/'>メモ作成</LinkText>
+		</div>
+	),
 };
