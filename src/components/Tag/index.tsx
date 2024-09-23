45887b0836121e4859e1bf7c8bdb61a93d1b0330
Conte0745 <r92720473@gmail.com>
2024-09-23 16:41:11 +0900

    Tagコンポーネントの余白削除

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Tag = ({ children }: Props) => {
 	return (
-		<span className='mb-1 ml-1 mr-1 rounded-full bg-gray-200 px-2.5 py-1 text-xs'>
+		<span className='rounded-full bg-gray-200 px-2.5 py-1 text-xs'>
 			<Text fontSize='s'>{children}</Text>
 		</span>
 	);
