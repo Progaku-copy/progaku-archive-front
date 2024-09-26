d644af824001c9276073359019acdebf03209a2c
Conte0745 <r92720473@gmail.com>
2024-09-27 00:04:55 +0900

    modalコンポーネント作成から IconButton をフェッチ

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -2,21 +2,48 @@
 
 import Text from '../Text';
 
+const ICON_SIZES = {
+	m: 'h-8 w-8',
+	s: 'h-4 w-4',
+} as const;
+
+const ICON_BUTTON_SIZES = {
+	m: 'h-10 w-10',
+	s: 'h-5 w-5',
+} as const;
+
+const ICON_JUSTIFIED = {
+	top: 'items-start',
+	center: 'items-center',
+} as const;
+
 type Props = {
 	label?: string;
+	size?: keyof typeof ICON_SIZES;
+	iconJustified?: keyof typeof ICON_JUSTIFIED;
 	iconType: 'edit' | 'delete';
-	onclickHandler: (...args: unknown[]) => unknown;
+	onClickHandler: (...args: unknown[]) => unknown;
 };
 
-const IconButton = ({ label, iconType, onclickHandler }: Props) => {
+const IconButton = ({
+	label = undefined,
+	size = 's',
+	iconJustified = 'center',
+	iconType,
+	onClickHandler,
+}: Props) => {
 	return (
-		<button
-			onClick={onclickHandler}
-			className='flex h-full items-center'
-		>
-			<span className={`i-ic-${iconType} m-0.5 h-8 w-8 p-2`}></span>
-			{label && <Text>{label}</Text>}
-		</button>
+		<div className={`h-15 ${label === undefined ? ICON_BUTTON_SIZES[size] : 'w-20'}`}>
+			<button
+				onClick={onClickHandler}
+				className={`flex ${ICON_JUSTIFIED[iconJustified]}`}
+			>
+				<span
+					className={`i-ic-${iconType} ${label === undefined ? ICON_BUTTON_SIZES[size] : 'm-0.5 p-2'} ${ICON_SIZES[size]}`}
+				></span>
+				{label && <Text fontSize='m'>{label}</Text>}
+			</button>
+		</div>
 	);
 };
 
