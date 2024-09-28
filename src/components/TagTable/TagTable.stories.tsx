9292f3fa3c74361eee3b62d63ed6f52a3e8468fa
Conte0745 <r92720473@gmail.com>
2024-09-29 01:20:34 +0900

    タグ一覧の説明文を削除

--- src/components/TagTable/TagTable.stories.tsx
+++ src/components/TagTable/TagTable.stories.tsx
@@ -5,45 +5,38 @@ import type { Meta, StoryObj } from '@storybook/react';
 type Tag = {
 	id: number;
 	name: string;
-	description: string;
 	priority: number;
 };
 
-const tags: Tag[] = [
+const DUMMY_TAGS: Tag[] = [
 	{
 		id: 1,
 		name: 'タグ1',
-		description: 'タグ1',
 		priority: 1,
 	},
 	{
 		id: 2,
 		name: 'タグ2',
-		description: 'タグ2',
 		priority: 2,
 	},
 	{
 		id: 3,
 		name: 'タグ3',
-		description: 'タグ3',
 		priority: 3,
 	},
 	{
 		id: 4,
 		name: 'タグ4',
-		description: 'タグ4',
 		priority: 4,
 	},
 	{
 		id: 5,
 		name: 'タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名',
-		description: 'タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名',
 		priority: 5,
 	},
 	{
 		id: 6,
-		name: 'タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名',
-		description: 'タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名',
+		name: 'タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名タグ名',
 		priority: 5,
 	},
 ];
@@ -58,6 +51,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
 	args: {
-		data: [],
+		tags: DUMMY_TAGS,
 	},
 };
--- src/components/TagTable/tagTable.tsx
+++ src/components/TagTable/tagTable.tsx
@@ -4,15 +4,14 @@ import IconButton from '../IconButton/index';
 type Tag = {
 	id: number;
 	name: string;
-	description: string;
 	priority: number;
 };
 
 type Props = {
-	data?: Tag[];
+	tags?: Tag[];
 };
 
-const TagTable = ({ data }: Props) => {
+const TagTable = ({ tags }: Props) => {
 	const editOnClickIcon = (id: number): any => {
 		console.log(`edit: ${id}`);
 	};
@@ -22,83 +21,35 @@ const TagTable = ({ data }: Props) => {
 
 	return (
 		<div className='flex justify-center'>
-			<div className='w-[660px]'>
-				<table className='w-full min-w-full max-w-lg table-fixed border-collapse border'>
-					<thead className='border-4 border-white bg-light-blue'>
-						<tr>
-							<th
-								className={`w-[200px] border-2 border-b-2 border-l-2 border-r-0 border-t-2 border-white p-1 py-3`}
-							>
-								<Text color='text-white'>タグ名</Text>
-							</th>
-							<th
-								className={`w-[400px] border-2 border-b-2 border-l-2 border-r-0 border-t-2 border-white p-1 py-3`}
-							>
-								<Text color='text-white'>説明</Text>
-							</th>
-							<th
-								className={`w-[30px] border-b-2 border-l-0 border-t-2 border-white py-3`}
-							>
-								{''}
-							</th>
-							<th
-								className={`w-[30px] border-b-2 border-l-0 border-t-2 border-white py-3`}
-							>
-								{''}
-							</th>
-						</tr>
-					</thead>
-					<tbody className='text-center'>
-						{data && data?.length > 0 ? (
-							data.map((row) => (
-								<tr
-									key={row.id}
-									className={`bg-gray-200 py-8 text-center`}
-								>
-									<td className='overflow-hidden text-ellipsis whitespace-nowrap border-2 border-b-2 border-l-2 border-r-2 border-t-2 border-white px-2 py-2'>
-										<Text>{row.name}</Text>
-									</td>
-									<td className='overflow-hidden text-ellipsis whitespace-nowrap border-2 border-b-2 border-l-2 border-r-0 border-t-2 border-white px-2 py-2'>
-										<Text>{row.description}</Text>
-									</td>
-									<td className='border-b-2 border-l-0 border-t-2 border-white align-middle'>
-										<div className='inline-block'>
-											<IconButton
-												iconType='edit'
-												size='s'
-												onClickHandler={() =>
-													editOnClickIcon(row.id)
-												}
-											></IconButton>
-										</div>
-									</td>
-									<td className='border-b-2 border-l-0 border-t-2 border-white align-middle'>
-										<div className='inline-block'>
-											<IconButton
-												iconType='delete'
-												size='s'
-												onClickHandler={() =>
-													deleteOnClickIcon(row.id)
-												}
-											></IconButton>
-										</div>
-									</td>
-								</tr>
-							))
-						) : (
-							<tr className='text-center`} bg-gray-200 py-8'>
-								<td className='border-2 border-b-2 border-l-2 border-r-0 border-t-2 border-white px-2 py-2'>
-									ー
-								</td>
-								<td className='border-b-2 border-l-2 border-t-2 border-white align-middle'>
-									ー
-								</td>
-								<td className='border-b-2 border-l-0 border-t-2 border-white align-middle'></td>
-								<td className='border-b-2 border-l-0 border-t-2 border-white align-middle'></td>
-							</tr>
-						)}
-					</tbody>
-				</table>
+			<div className='w-4/5 space-y-2'>
+				{tags &&
+					tags?.length > 0 &&
+					tags.map((tag) => (
+						<div
+							key={tag.id}
+							className='flex justify-between bg-gray-200 px-5'
+						>
+							<div className='m-2 flex flex-wrap p-1'>
+								<Text>{tag.name}</Text>
+							</div>
+							<div className='flex justify-end'>
+								<div className='m-2'>
+									<IconButton
+										iconType='edit'
+										size='s'
+										onClickHandler={() => editOnClickIcon(tag.id)}
+									></IconButton>
+								</div>
+								<div className='m-2'>
+									<IconButton
+										iconType='delete'
+										size='s'
+										onClickHandler={() => deleteOnClickIcon(tag.id)}
+									></IconButton>
+								</div>
+							</div>
+						</div>
+					))}
 			</div>
 		</div>
 	);
