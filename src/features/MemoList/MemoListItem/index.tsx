e04da8ffcb0077aae80e8687fc6fa82c6569c7aa
Conte0745 <r92720473@gmail.com>
2024-09-29 00:25:38 +0900

    メモアイテムに表示する日付を dateLabel を使用するように修正

--- src/features/MemoList/MemoListItem/index.tsx
+++ src/features/MemoList/MemoListItem/index.tsx
@@ -1,3 +1,4 @@
+import DateLabel from '@/components/DateLabel';
 import { Memo } from '@/Types';
 import Tag from '@/components/Tag';
 import Text from '@/components/Text';
@@ -10,9 +11,12 @@ type Props = {
 const MemoListItem = ({ memo }: Props) => {
 	return (
 		<div className='flex flex-col justify-center gap-2 rounded-lg bg-gray-100 p-3'>
-			<Text fontSize='l'>{memo.poster}</Text>
-			<Text fontSize='s'>{memo.created_at}</Text>
-			<Title isBold>{memo.title}</Title>
+			<Text fontSize='l'>{userName}</Text>
+			<DateLabel
+				fontSize='s'
+				utcDateTimeString={createdDate}
+			/>
+			<Title isBold>{memoTitle}</Title>
 			<div className='flex flex-wrap'>
 				{memo.tags.map((tag) => {
 					return (
