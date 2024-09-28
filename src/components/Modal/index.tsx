1b08f31da4ce3f412e7e3280a9a691757ab6394c
Conte0745 <r92720473@gmail.com>
2024-09-29 00:42:15 +0900

    説明の削除

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,8 +1,7 @@
 import Input from '@/components/Input';
-import TextArea from '@/components/TextArea';
 import Title from '@/components/Title';
-import { useState } from 'react';
 import ColorButton from '../ColorButton';
+import { useState } from 'react';
 
 type Props = {
 	onClose: () => void;
@@ -11,33 +10,35 @@ type Props = {
 
 const Modal = ({ onClose, mode }: Props) => {
 	const [name, setName] = useState('');
-	const [description, setDescription] = useState('');
+	const onClickSubmitButton = () => {
+		console.log('submit');
+	};
 
 	return (
 		<div className='z-10 mx-auto max-w-4xl flex-col rounded-full'>
-			<div className='px-38 m-10 rounded-lg border border-gray-400 p-10'>
+			<div className='px-38 m-10 space-y-5 rounded-lg border border-gray-400 p-10'>
 				<div className='flex justify-between'>
 					<Title
 						fontSize='l'
 						isBold
 					>
-						{mode === 'edit' ? 'タグの編集' : 'タグの作成'}
+						{mode === 'edit' ? 'タグを編集' : 'タグを作成'}
 					</Title>
 					<button
 						className='i-ic-close px-3 py-3 text-gray-500 hover:opacity-75'
 						onClick={onClose}
 					></button>
 				</div>
-				<br />
-				<br />
-				<Input placeholder='タグ名' />
-				<br />
-				<TextArea placeholder='説明' />
-				<br />
+				<Input
+					placeholder='タグ名'
+					setInputValue={setName}
+					inputValue={name}
+				/>
 				<div className='flex justify-end'>
 					<ColorButton
 						label='送信'
 						color='green'
+						handleClickButton={onClickSubmitButton}
 					></ColorButton>
 				</div>
 			</div>
