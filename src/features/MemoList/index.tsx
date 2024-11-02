ffa264142a98295827094ec6558f64e2c752e6c0
Conte0745 <r92720473@gmail.com>
2024-11-02 20:37:32 +0900

    デザイン修正

--- src/features/MemoList/index.tsx
+++ src/features/MemoList/index.tsx
@@ -28,35 +28,39 @@ export function MemoList({
 }: Props) {
 	return (
 		<div className='mt-10 flex flex-col justify-center gap-8 px-48'>
-			<div className=''>
-				<div className='m-1 w-full'>
-					<MultipleSelectInput
-						selectedChips={selectedChips}
-						setSelectedChips={setSelectedChips}
-						labelPosition='left'
-						selectOptions={tags}
-						placeholder='タグを入力してください'
-						label='タグ検索'
-					/>
+			<div>
+				<div className='m-1 flex items-center'>
+					<div className='mr-5 w-4/5'>
+						<MultipleSelectInput
+							selectedChips={selectedChips}
+							setSelectedChips={setSelectedChips}
+							labelPosition='left'
+							selectOptions={tags}
+							placeholder='タグを入力してください'
+							label='タグ検索'
+						/>
+					</div>
+					<div className='w-20'></div>
 				</div>
-				<div className='m-1 w-full'>
-					<Input
-						label='メモ検索'
-						placeholder='メモを入力してください'
-						labelPosition='left'
-						setInputValue={setSearchMemoContent}
-						inputValue={searchMemoContent}
-					/>
+				<div className='m-1 flex items-center'>
+					<div className='mr-5 w-4/5'>
+						<Input
+							label='メモ検索'
+							placeholder='メモを入力してください'
+							labelPosition='left'
+							setInputValue={setSearchMemoContent}
+							inputValue={searchMemoContent}
+						/>
+					</div>
+					<div className='w-20'>
+						<ColorButton
+							color='green'
+							label='検索'
+							handleClickButton={searchButtonClicked}
+						/>
+					</div>
 				</div>
 			</div>
-			<div className='text-center'>
-				<ColorButton
-					color='green'
-					label='検索'
-					handleClickButton={searchButtonClicked}
-				/>
-			</div>
-			<span className='border'></span>
 			<div className='space-y-5'>
 				{memos.map((memo) => {
 					return (
