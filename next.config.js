8f9c3dee87c67ee5a170ec2faf0f51b2123a8f68
日下陸矢 <rikuya.98@gmail.com>
2025-03-16 23:25:55 +0900

    メモリ不足でビルドに落ちるので最適化

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	experimental: {
+		optimizeCss: true,
+		optimizeFonts: true,
+	},
+};
 
 module.exports = nextConfig;
