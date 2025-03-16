05ce794baae0091483c37649bc8173899fdfe923
日下陸矢 <rikuya.98@gmail.com>
2025-03-16 22:26:27 +0900

    [fix]エンターでもフォームを送信できるように、auto completeを有効に

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,6 +8,11 @@ type Props = {
 	labelPosition?: 'left' | 'top';
 	setInputValue: (value: string) => void;
 	inputValue: string;
+	type?: 'text' | 'password';
+	onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+	name?: string;
+	autoComplete?: string;
+	className?: string;
 };
 
 const Input = ({
@@ -16,18 +21,29 @@ const Input = ({
 	labelPosition = 'top',
 	setInputValue,
 	inputValue,
+	type = 'text',
+	onKeyDown,
+	name,
+	autoComplete,
+	className = '',
 }: Props) => {
 	return (
-		<div className={labelPosition === 'left' ? 'flex items-center' : ''}>
+		<div
+			className={`${labelPosition === 'left' ? 'flex items-center' : ''} ${className}`}
+		>
 			<div className={labelPosition === 'left' ? 'w-32' : ''}>
 				{!!label && <Text>{label}</Text>}
 			</div>
 			<div className={labelPosition === 'left' ? 'w-full' : ''}>
 				<input
-					className='h-11 w-full rounded-lg border-2 border-light-gray px-2'
+					className='h-11 w-full rounded-lg border border-gray-200 px-3 focus:border-light-blue focus:outline-none'
 					placeholder={placeholder}
 					value={inputValue}
+					type={type}
 					onChange={(e) => setInputValue(e.target.value)}
+					onKeyDown={onKeyDown}
+					name={name}
+					autoComplete={autoComplete}
 				></input>
 			</div>
 		</div>
